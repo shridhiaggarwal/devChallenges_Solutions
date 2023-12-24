aca0793de763ac3b29c8a5aa819b60538256ebee
shridhiaggarwal <shridhi.aggarwal@gmail.com>
2023-12-25 01:38:49 +0530

    update in script file challenge 5

--- challenges/challenge5_simpleHomePage/script.js
+++ challenges/challenge5_simpleHomePage/script.js
@@ -6,14 +6,17 @@ document.addEventListener("DOMContentLoaded", function () {
     let bannerByliner = document.getElementById("bannerByliner");
     let bannerHeading = document.getElementById("bannerHeading");
 
-    console.log("themeToggleBtn.checked", themeToggleBtn.checked)
+    // console.log("themeToggleBtn.checked", themeToggleBtn.checked);
 
     navItems.forEach((item) => {
         item.addEventListener('click', () => {
             activeItem.classList.remove('navItemActive');
+            if (themeToggleBtn.checked) {
+                activeItem.classList.remove("darkThemeText");
+            }
             item.classList.add('navItemActive');
             if (themeToggleBtn.checked) {
-                item.style.color = "#F2F9FE";
+                item.classList.add("darkThemeText");
             }
             activeItem = item;
         })
@@ -22,14 +25,14 @@ document.addEventListener("DOMContentLoaded", function () {
     themeToggleBtn.addEventListener('change', () => {
         if (themeToggleBtn.checked) {
             // dark theme styling
-            this.body.style.backgroundColor = "#111729";
+            this.body.classList.add("darkThemeBody");
             logoImage.src = "../../images/challenge5/alarado-icon-homepage-light.svg";
             activeItem.classList.add("darkThemeText");
             bannerByliner.classList.add("darkThemeText");
             bannerHeading.classList.add("darkThemeText");
         } else {
             //default styleing - light theme
-            this.body.style.backgroundColor = "#F2F9FE";
+            this.body.classList.remove("darkThemeBody");
             logoImage.src = "../../images/challenge5/alarado-icon-homepage.svg";
             activeItem.classList.remove("darkThemeText");
             bannerByliner.classList.remove("darkThemeText");
