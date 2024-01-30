4d8f400621380fb2af9bfbdc07864113b50e7a25
shridhiaggarwal <shridhi.aggarwal@gmail.com>
2024-01-30 23:38:17 +0530

    challenge 6 - qr code page

--- challenges/challenge6_qrCodeGenerator/script.js
+++ challenges/challenge6_qrCodeGenerator/script.js
@@ -1,33 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const mainTag = document.getElementsByTagName('main')[0];
+    const headerPart = document.getElementById('headerPart');
+    const qrCode = document.getElementById("qrcode");
+    const searchPagePart = document.getElementById('searchPagePart');
+    const loaderPagePart = document.getElementById('loaderPagePart');
+    const qrCodePagePart = document.getElementById('qrCodePagePart');
+
     // Function to generate QR code
     function generateQRCode() {
         // Hide search page part - part 1
-        document.getElementById('searchPagePart').style.display = 'none';
+        searchPagePart.style.display = 'none';
 
         // Show Loader page - part 2
-        document.getElementById('loaderPagePart').style.display = 'flex';
+        loaderPagePart.style.display = 'flex';
+        mainTag.style.height = 'inherit';
+
 
         // Get the data (you can replace this with your desired data)
         const dataToEncode = "Hello, QR Code!";
 
         // Clear any existing QR code
-        document.getElementById("qrcode").innerHTML = "";
+        qrCode.innerHTML = "";
 
         // Create a new instance of QRCode and generate the QR code
-        const qrcode = new QRCode(document.getElementById("qrcode"), {
+        const qrcode = new QRCode(qrCode, {
             text: dataToEncode,
-            width: 128,
-            height: 128,
+            width: 184,
+            height: 184,
         });
 
         // Wait for 5 seconds
         setTimeout(function () {
             // Hide Loader page part - part 2
-            document.getElementById('loaderPagePart').style.display = 'none';
+            loaderPagePart.style.display = 'none';
+            mainTag.style.height = 'auto';
 
             // Show QR code page part - part 3
             if (qrcode) {
-                document.getElementById('qrCodePagePart').style.display = 'flex';
+                qrCodePagePart.style.display = 'flex';
+                headerPart.style.display = "block";
             }
         }, 5000);
     }
